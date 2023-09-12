b5e81fdbae1b0d6e694733243bc815423ad07974
Axel Curipaco <axel@cibertec.edu.pe>
2023-09-11 22:05:02 -0500

    Validar Password

--- src/main/java/main/Pruebas.java
+++ src/main/java/main/Pruebas.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import model.TbUsuario;
 
@@ -16,9 +17,25 @@ System.out.print("===JPACRUD===");
 		
 		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("JPA_clase01x");
 		EntityManager em = fabrica.createEntityManager();
-		
+		/*
 		List<TbUsuario> lstUsuario = em.createQuery("Select a From TbUsuario a",TbUsuario.class).getResultList();
 		System.out.print("El Número de Usuarios es :"+ lstUsuario.size());
+		*/
+		
+		Query query = em.createNativeQuery("{call usp_validaAcceso(?,?)}",TbUsuario.class);
+		query.setParameter(1, "U001@gmail.com");
+		query.setParameter(2, "10001");
+		TbUsuario u = (TbUsuario) query.getSingleResult();
+		if (u == null) {
+		System.out.println("Usuario no existe");
+		}else{
+			System.out.println("Usuario  existe");
+		}
+		System.out.println("Bienvenido " + u.getNomUsua());
+		System.out.println("Tus datos son : " +u);
+		List<TbUsuario> lstUsuarios= query.getResultList();
+		System.out.print("El Número de Usuarios es :"+ lstUsuarios.size());
+		
 	}
 
 }
